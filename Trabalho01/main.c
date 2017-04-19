fecffd08a9b500729bb95acb5febd9326b2131ac
GabrielScalici <gabrielhc.scalici@gmail.com>
2017-04-19 08:53:42 -0300

    Posicao da helice ajustada

--- Trabalho01/main.c
+++ Trabalho01/main.c
@@ -22,19 +22,16 @@
 
 
 //Metodo de evento do mouse
-GLfloat R=0.0f, G=0.0f, B=0.0f;
+GLfloat R=0.0f, G=0.0f, B=0.0f, angle = 0.0f;
+
 
 void on_mouseClick(int botao_clicado, int estado_do_click, int x_mouse_position, int y_mouse_position) {
     
     if (estado_do_click == GLUT_DOWN) {
         if (botao_clicado == GLUT_RIGHT_BUTTON) {
-            R = 0.0f;
-            G = 0.0f;
-            B = 0.0f;
+
         } else if (botao_clicado == GLUT_LEFT_BUTTON) {
-            R = 1.0f;
-            G = 1.0f;
-            B = 1.0f;
+
         }
     }
     
@@ -56,36 +53,36 @@ void desenha(){
     ///////////////////////////////
     
     
-    // Especifica a cor preta
+    // Especifica a cor
     glColor3f(0.18f, 0.31f, 0.31f);
     
     // Desenha o triangulo
     glBegin(GL_TRIANGLES);
-    glVertex2f(100.0f, 30.0f);
-    glVertex2f(110.0f, 120.0f);
-    glVertex2f(120.0f, 30.0f);
+    glVertex2f(90.0f, 30.0f);
+    glVertex2f(100.0f, 120.0f);
+    glVertex2f(110.0f, 30.0f);
     glEnd();
 
-    // Especifica a cor preta
+    // Especifica a cor
     glColor3f(0.44f, 0.5f, 0.56f);
     
     // Desenha um retângulo preenchido com a cor corrente
     glBegin(GL_POLYGON);
-    glVertex2f(0.0f, 50.0f);
-    glVertex2f(100.0f, 50.0f);
-    glVertex2f(100.0f, 80.0f);
-    glVertex2f(0.0f, 80.0f);
+    glVertex2f(95.0f, 70.0f);
+    glVertex2f(103.0f, 70.0f);
+    glVertex2f(103.0f, 160.0f);
+    glVertex2f(95.0f, 160.0f);
     glEnd();
     
-    // Especifica a cor preta
-    glColor3f(R, G, B);
+    // Especifica a cor
+    glColor3f(0.44f, 0.5f, 0.56f);
     
     // Desenha um retângulo preenchido com a cor corrente
     glBegin(GL_POLYGON);
-    glVertex2f(50.0f, 60.0f);
-    glVertex2f(150.0f, 60.0f);
-    glVertex2f(150.0f, 80.0f);
-    glVertex2f(50.0f, 80.0f);
+    glVertex2f(60.0f, 110.0f);
+    glVertex2f(140.0f, 110.0f);
+    glVertex2f(140.0f, 120.0f);
+    glVertex2f(60.0f, 120.0f);
     glEnd();
     
     
@@ -94,15 +91,13 @@ void desenha(){
 }
 
 
-
-
 int main(int argc, const char * argv[]) {
     
     glutInit(&argc, argv); // Inicia uma inst�ncia da glut
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // Define o modo de display do buffer
     glutInitWindowSize(800, 600); // Define o tamanho em pixels da janela
     glutCreateWindow("Ventilador"); // Define o t�tulo da janela
-    glutDisplayFunc(desenha); // Estabelece que a fun��o de rendering � a fun��o "desenha(.)"
+     // Estabelece que a fun��o de rendering � a fun��o "desenha(.)"
     
     
     glutMouseFunc(on_mouseClick); // Evento de click do mouse
@@ -111,7 +106,11 @@ int main(int argc, const char * argv[]) {
     glMatrixMode(GL_PROJECTION); // Especifica��es de observa��o de cena
     gluOrtho2D(0, 200, 0, 200);	// Observa-se a se��o de plano [0,200]x[0,200]
     
+    glutDisplayFunc(desenha);
+    
     
     glutMainLoop();
+    
+    
     
 }

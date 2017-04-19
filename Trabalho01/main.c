2cbea4e145dfe658ee637f004f1126469b2f5552
GabrielScalici <gabrielhc.scalici@gmail.com>
2017-04-19 10:48:23 -0300

    Rotacao adicionada

--- Trabalho01/main.c
+++ Trabalho01/main.c
@@ -22,16 +22,16 @@
 
 
 //Metodo de evento do mouse
-GLfloat R=0.0f, G=0.0f, B=0.0f, angle = 0.0f;
+GLfloat R=0.0f, G=0.0f, B=0.0f, angulo = 0.0f;
 
 
 void on_mouseClick(int botao_clicado, int estado_do_click, int x_mouse_position, int y_mouse_position) {
     
     if (estado_do_click == GLUT_DOWN) {
         if (botao_clicado == GLUT_RIGHT_BUTTON) {
-
+            angulo -= 4;
         } else if (botao_clicado == GLUT_LEFT_BUTTON) {
-
+            angulo += 4;
         }
     }
     
@@ -41,12 +41,13 @@ void on_mouseClick(int botao_clicado, int estado_do_click, int x_mouse_position,
 
 //Funcoes auxiliares
 void desenha(){
-
+    
+    
     // Colorir o fundo de branco
     glClearColor(0.28f, 0.82f, 0.8f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-
+    
     // Iniciar as transforma��es 2D
     glMatrixMode(GL_MODELVIEW); // Inicia-se a matriz de transforma��es da openGL
     glLoadIdentity();
@@ -56,57 +57,95 @@ void desenha(){
     // Especifica a cor
     glColor3f(0.18f, 0.31f, 0.31f);
     
+    
     // Desenha o triangulo
     glBegin(GL_TRIANGLES);
-    glVertex2f(90.0f, 30.0f);
+    glVertex2f(90.0f, 20.0f);
     glVertex2f(100.0f, 120.0f);
-    glVertex2f(110.0f, 30.0f);
+    glVertex2f(110.0f, 20.0f);
     glEnd();
-
+    
+    glLoadIdentity();
     // Especifica a cor
     glColor3f(0.44f, 0.5f, 0.56f);
     
+    if(angulo > 360){
+        angulo = 0;
+    }
+    
+    float midX = (95.0 * 2 + 103.0 *2)/4;
+    float midY = (70.0 * 2 + 160.0 * 2)/4;
+    
+    glTranslatef(midX, midY, 0.0f);
+    glRotatef(angulo, 0.0f, 0.0f, 1.0f);
+    glTranslatef(-midX, -midY, 0.0f);
+    
     // Desenha um retângulo preenchido com a cor corrente
     glBegin(GL_POLYGON);
-    glVertex2f(95.0f, 70.0f);
-    glVertex2f(103.0f, 70.0f);
-    glVertex2f(103.0f, 160.0f);
-    glVertex2f(95.0f, 160.0f);
+        glVertex2f(95.0f, 70.0f);
+        glVertex2f(105.0f, 70.0f);
+        glVertex2f(105.0f, 160.0f);
+        glVertex2f(95.0f, 160.0f);
     glEnd();
     
+    glLoadIdentity();
     // Especifica a cor
     glColor3f(0.44f, 0.5f, 0.56f);
     
+    midX = (95.0 * 2 + 103.0 *2)/4;
+    midY = (70.0 * 2 + 160.0 * 2)/4;
+    
+    glTranslatef(midX, midY, 0.0f);
+    glRotatef(angulo, 0.0f, 0.0f, 1.0f);
+    glTranslatef(-midX, -midY, 0.0f);
+    
     // Desenha um retângulo preenchido com a cor corrente
     glBegin(GL_POLYGON);
-    glVertex2f(60.0f, 110.0f);
-    glVertex2f(140.0f, 110.0f);
-    glVertex2f(140.0f, 120.0f);
-    glVertex2f(60.0f, 120.0f);
-    glEnd();
     
+        glVertex2f(60.0f, 110.0f);
+        glVertex2f(140.0f, 110.0f);
+        glVertex2f(140.0f, 120.0f);
+        glVertex2f(60.0f, 120.0f);
+    glEnd();
     
     glFlush();
+    
+}
+
+void atualizar(){
+    // Background color
+    glClearColor(0.28f, 0.82f, 0.8f, 1.0f);
+    glColor3f(1.0f, 1.0f, 1.0f);
+    
+    // Repaint screen
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    // Draw scene
+    desenha();
+    
+    glutSwapBuffers();
+
     
 }
 
 
 int main(int argc, const char * argv[]) {
     
-    glutInit(&argc, argv); // Inicia uma inst�ncia da glut
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // Define o modo de display do buffer
-    glutInitWindowSize(800, 600); // Define o tamanho em pixels da janela
-    glutCreateWindow("Ventilador"); // Define o t�tulo da janela
-     // Estabelece que a fun��o de rendering � a fun��o "desenha(.)"
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("Ventilador");
+
+    
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(0, 200, 0, 200);
     
     
-    glutMouseFunc(on_mouseClick); // Evento de click do mouse
+    //Funcoes para desenhar na tela
+    glutDisplayFunc(atualizar);
     
-    
-    glMatrixMode(GL_PROJECTION); // Especifica��es de observa��o de cena
-    gluOrtho2D(0, 200, 0, 200);	// Observa-se a se��o de plano [0,200]x[0,200]
-    
-    glutDisplayFunc(desenha);
+    //Funcoes para identificar o clique do mouse
+    glutMouseFunc(on_mouseClick);
     
     
     glutMainLoop();

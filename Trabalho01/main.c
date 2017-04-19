ec949173ea87447a7eb400d11f2001d5cccfffa2
GabrielScalici <gabrielhc.scalici@gmail.com>
2017-04-19 14:24:38 -0300

    Adicao de fisica simplificada

--- Trabalho01/main.c
+++ Trabalho01/main.c
@@ -24,18 +24,36 @@
 //Metodo de evento do mouse
 GLfloat R=0.0f, G=0.0f, B=0.0f, angulo = 0.0f;
 
+GLfloat vel = 0.0f, vellimit = 6.0f, accel = 3.0f, air = 0.1f;
+
 
 void on_mouseClick(int botao_clicado, int estado_do_click, int x_mouse_position, int y_mouse_position) {
     
     if (estado_do_click == GLUT_DOWN) {
         if (botao_clicado == GLUT_RIGHT_BUTTON) {
-            angulo -= 4;
+            vel -= accel;
         } else if (botao_clicado == GLUT_LEFT_BUTTON) {
-            angulo += 4;
+            vel += accel;
         }
     }
     
     glutPostRedisplay(); // For�a a glut redesenhar a cena ap�s a atualiza��o.
+}
+
+void redesenha(int height, int width){
+    if (height == 0)
+        height = 1;
+    if (width == 0)
+        width = 1;
+    float aspect = width * 1.0 / height;
+    
+    glMatrixMode(GL_VIEWPORT);
+    glLoadIdentity();
+    
+    glViewport(0, 0, width, height);
+    gluOrtho2D(-100 * aspect, 100 * aspect, -100, 100);
+    
+    glMatrixMode(GL_MODELVIEW);
 }
 
 
@@ -57,24 +75,44 @@ void desenha(){
     // Especifica a cor
     glColor3f(0.18f, 0.31f, 0.31f);
     
+    glTranslatef(-30.0f, -115.0f, 0.0f);
     
     // Desenha o triangulo
     glBegin(GL_TRIANGLES);
-    glVertex2f(90.0f, 20.0f);
-    glVertex2f(100.0f, 120.0f);
-    glVertex2f(110.0f, 20.0f);
+    glVertex2f(40.0f, 20.0f);
+    glVertex2f(50.0f, 120.0f);
+    glVertex2f(60.0f, 20.0f);
     glEnd();
     
     glLoadIdentity();
     // Especifica a cor
     glColor3f(0.44f, 0.5f, 0.56f);
+    
+    if(vel <= -air)
+        vel = vel + air;
+    else if (vel >= air)
+        vel = vel - air;
+    else if (vel > -air && vel < air)
+        vel = 0;
+    
+    if(vel > vellimit)
+        vel = vellimit;
+    else if(vel < - vellimit)
+        vel = -vellimit;
+    
+    angulo = angulo + vel;
     
     if(angulo > 360){
         angulo = 0;
     }
+    else if(angulo < 0){
+        angulo = 360;
+    }
     
-    float midX = (95.0 * 2 + 103.0 *2)/4;
+    float midX = (45.0 * 2 + 55.0 *2)/4;
     float midY = (70.0 * 2 + 160.0 * 2)/4;
+    
+    glTranslatef(-30.0f, -115.0f, 0.0f);
     
     glTranslatef(midX, midY, 0.0f);
     glRotatef(angulo, 0.0f, 0.0f, 1.0f);
@@ -82,30 +120,28 @@ void desenha(){
     
     // Desenha um retângulo preenchido com a cor corrente
     glBegin(GL_POLYGON);
-        glVertex2f(95.0f, 70.0f);
-        glVertex2f(105.0f, 70.0f);
-        glVertex2f(105.0f, 160.0f);
-        glVertex2f(95.0f, 160.0f);
+        glVertex2f(45.0f, 70.0f);
+        glVertex2f(55.0f, 70.0f);
+        glVertex2f(55.0f, 160.0f);
+        glVertex2f(45.0f, 160.0f);
     glEnd();
     
     glLoadIdentity();
     // Especifica a cor
     glColor3f(0.44f, 0.5f, 0.56f);
     
-    midX = (95.0 * 2 + 103.0 *2)/4;
-    midY = (70.0 * 2 + 160.0 * 2)/4;
+    glTranslatef(-30.0f, -115.0f, 0.0f);
     
     glTranslatef(midX, midY, 0.0f);
-    glRotatef(angulo, 0.0f, 0.0f, 1.0f);
+    glRotatef(angulo + 90, 0.0f, 0.0f, 1.0f);
     glTranslatef(-midX, -midY, 0.0f);
     
     // Desenha um retângulo preenchido com a cor corrente
     glBegin(GL_POLYGON);
-    
-        glVertex2f(60.0f, 110.0f);
-        glVertex2f(140.0f, 110.0f);
-        glVertex2f(140.0f, 120.0f);
-        glVertex2f(60.0f, 120.0f);
+        glVertex2f(45.0f, 70.0f);
+        glVertex2f(55.0f, 70.0f);
+        glVertex2f(55.0f, 160.0f);
+        glVertex2f(45.0f, 160.0f);
     glEnd();
     
     glFlush();
@@ -124,32 +160,31 @@ void atualizar(){
     desenha();
     
     glutSwapBuffers();
-
-    
 }
 
 
 int main(int argc, const char * argv[]) {
     
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    //glEnable(GL_MULTISAMPLE);
     glutInitWindowSize(800, 600);
     glutCreateWindow("Ventilador");
-
     
     glMatrixMode(GL_PROJECTION);
-    gluOrtho2D(0, 200, 0, 200);
+    glViewport(0, 0, 800, 600);
+    gluOrtho2D(-100, 100, -100, 100);
     
     
     //Funcoes para desenhar na tela
     glutDisplayFunc(atualizar);
+    glutIdleFunc(atualizar);
+    glutReshapeFunc(redesenha);
     
     //Funcoes para identificar o clique do mouse
     glutMouseFunc(on_mouseClick);
     
     
     glutMainLoop();
-    
-    
     
 }
